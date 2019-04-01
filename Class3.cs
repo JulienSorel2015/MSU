e7d34ea555bce4217cc2bb7549ac8f5ea35a01bd
JulienSorel2015 <ademozay@gmail.com>
2019-04-01 18:46:09 +0300

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+
+//using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+//using Autodesk.Revit.DB.ExtensibleStorage;
+using Autodesk.Revit.UI.Selection;
+
+namespace FirstPlugin
+{
+    class Class3
+    {
+        
+    }
+    public class ModelLineSelectionFilter : ISelectionFilter
+    {
+        
+        public bool AllowElement(Element element)
+        {
+            if ((element.Category.Name == "Lines"))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool AllowReference(Reference refer, XYZ point)
+        {
+            return false;
+        }
+    }
+}
